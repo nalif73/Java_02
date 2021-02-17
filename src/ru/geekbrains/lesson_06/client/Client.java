4da7e460e713e07bdb270faa990dc863477c9cbd
Nalif <nalif@mail.ru>
2021-02-17 11:16:35 +0300

    commit

---
+++ src/ru/geekbrains/lesson_06/client/Client.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson_06.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+}
---
+++ src/ru/geekbrains/lesson_06/server/Server.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson_06.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+
+}
