7cfcac69d6c9d96b1773de86d89af5792d48271a
Nalif <nalif@mail.ru>
2021-02-18 13:56:15 +0300

    Lesson-06 final

--- src/ru/geekbrains/lesson_06/client/Client.java
+++ src/ru/geekbrains/lesson_06/client/Client.java
@@ -7,5 +7,42 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
 
+    public Client() {
+        try {
+            socket = new Socket("127.0.0.1", 8989);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+
+            new Thread(() -> {
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    try {
+                        System.out.println("Please input message...");
+                        out.writeUTF(scanner.nextLine());
+                    } catch (Exception e) {
+                        System.out.println("Transmission closed.");
+                        break;
+                    }
+                }
+            })
+                    .start();
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        System.out.println("Message from server: " + in.readUTF());
+                    } catch (IOException e) {
+                        System.out.println("Connection closed.");
+                        break;
+                    }
+                }
+            })
+                    .start();
+        } catch (IOException e) {
+            throw new RuntimeException("SWW", e);
+        }
+    }
 }
--- src/ru/geekbrains/lesson_06/server/Server.java
+++ src/ru/geekbrains/lesson_06/server/Server.java
@@ -8,5 +8,45 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
+    private ServerSocket serverSocket;
+    private DataInputStream in;
+    private DataOutputStream out;
 
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(8989);
+            System.out.println("Server is running on and waiting for a connection...");
+            Socket client = serverSocket.accept();
+            System.out.println("Client accepted: " + client);
+            in = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+
+            new Thread(() -> {
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    try {
+                        System.out.println("Please input message...");
+                        out.writeUTF(scanner.nextLine());
+                    } catch (Exception e) {
+                        System.out.println("Transmission closed.");
+                        break;
+                    }
+                }
+            })
+                    .start();
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        System.out.println("Message from client: " + in.readUTF());
+                    } catch (IOException e) {
+                        System.out.println("Connection closed.");
+                        break;
+                    }
+                }
+            })
+                    .start();
+        } catch (IOException e) {
+            throw new RuntimeException("SWW", e);
+        }
+    }
 }
